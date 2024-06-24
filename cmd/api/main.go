7ba6659b7546fc2f8792fd152c520b848b689a96
Junior Paula <luke.junnior@icloud.com>
2024-06-24 18:53:27 -0300

    feat: configurate gracefull shutting down

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"xml-reader-api/internal/config"
 	"xml-reader-api/internal/routes"
 )
@@ -13,6 +18,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	// create context to control the lifetime of the server
+	_, cancel := context.WithCancel(context.Background())
+
 	// Create a new database connection
 	db, err := config.ConnectDatabase(cfg.DB_NAME)
 	if err != nil {
@@ -22,9 +34,31 @@ func main() {
 	// Automatically migrate the database
 	config.AutoMigrate(db)
 
-	routes := routes.NewRoutes(db, cfg)
+	server := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: routes.NewRoutes(db, cfg),
+	}
 
-	// Listen and serve the API
-	fmt.Printf("Server running on port %s\n", cfg.Port)
-	log.Fatal(http.ListenAndServe(":"+cfg.Port, routes))
+	go func() {
+		fmt.Printf("Server running on port %s\n", cfg.Port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error while start server\n: %v", err)
+		}
+	}()
+
+	// wait for the interrupt signal
+	<-interrupt
+	fmt.Println("Shutting down server...")
+	cancel()
+
+	timeout := 5 * time.Second
+	ctxShutDown, cancelShutDown := context.WithTimeout(context.Background(), timeout)
+	defer func() {
+		cancelShutDown()
+	}()
+
+	if err := server.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("Server shutdown failed:%+v", err)
+	}
+	fmt.Print("Server exiting...\n")
 }
